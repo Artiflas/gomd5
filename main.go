615eb87f001f4fb0b818e859872ebd7be171b3f1
Jeremy Artiflas <Artiflas@gmail.com>
2021-04-11 12:38:19 +0200

    Implemented a test

--- main.go
+++ main.go
@@ -2,16 +2,40 @@ package main
 
 import (
 	"crypto/md5"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
+var flagFile = flag.String("file", "", "file used for input")
+
+var input io.Reader
+
 func main() {
+	flag.Parse()
+	switch {
+	case *flagFile != "":
+		f, err := os.Open(*flagFile)
+		if err != nil {
+			fmt.Println("error opening file:", *flagFile, err)
+			os.Exit(123)
+		}
+		defer f.Close()
+		input = f
+	default:
+		input = os.Stdin
+	}
+	printMD5(input, os.Stdout)
+
+}
+
+func printMD5(r io.Reader, w io.Writer) {
 	h := md5.New()
-	if _, err := io.Copy(h, os.Stdin); err != nil {
+	if _, err := io.Copy(h, r); err != nil {
 		fmt.Println("error copy data to hash")
 		os.Exit(1)
 	}
-	fmt.Printf("%x", h.Sum(nil))
+
+	fmt.Fprintf(w, "%x", h.Sum(nil))
 }
---
+++ main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+
+
+func TestPrintMD5(t *testing.T) {
+	in := strings.NewReader("golang")
+	out := &bytes.Buffer{}
+	printMD5(in, out)
+	want := "21cc28409729565fc1a4d2dd92db269f"
+	got := out.String()
+	if got != want {
+		t.Errorf("The md5 hash is faulty")
+	}
+
+}
